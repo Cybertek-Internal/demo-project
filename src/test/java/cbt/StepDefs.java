cab74ae8f2bc38af06bd3e3f8a9580e752f3b94d
Vasyl Fomiuk <vasyl@cybertekschool.com>
2019-08-22 21:59:51 -0400

    update

--- src/test/java/cbt/StepDefs.java
+++ src/test/java/cbt/StepDefs.java
@@ -19,35 +19,36 @@ import java.util.concurrent.TimeUnit;
 
 public class StepDefs {
 
-	@Given("^I am on the home page$")
-	public void i_am_on_the_home_page() throws Throwable {
-		Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		Driver.getDriver().get("http://etsy.com");
+    @Given("^I am on the home page$")
+    public void i_am_on_the_home_page() throws Throwable {
+        Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().get("http://etsy.com");
 
-	}
+    }
 
-	@When("^I search for \"([^\"]*)\"$")
-	public void i_search_for(String search) throws Throwable {
-		Driver.getDriver().findElement(By.cssSelector("[id*='search-query']")).sendKeys(search + Keys.ENTER);
-	}
+    @When("^I search for \"([^\"]*)\"$")
+    public void i_search_for(String search) throws Throwable {
+        Driver.getDriver().findElement(By.cssSelector("[id*='search-query']")).sendKeys(search + Keys.ENTER);
+    }
 
-	@Then("^I should see the results$")
-	public void i_should_see_the_results() throws Throwable {
-		Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
-	}
+    @Then("^I should see the results$")
+    public void i_should_see_the_results() throws Throwable {
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
+    }
 
-	@Then("^I should see more results$")
-	public void i_should_see_more_results() throws Throwable {
-		Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
-	}
+    @Then("^I should see more results$")
+    public void i_should_see_more_results() throws Throwable {
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
+    }
 
-	@After
-	public void tearDown(Scenario scenario) {
-		if (scenario.isFailed()) {
-			final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-			scenario.embed(screenshot, "image/png");
-		}
-		Driver.closeDriver();
-	}
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+        Driver.closeDriver();
+    }
 
 }
